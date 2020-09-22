ed5d5728cfbbf9d5ff15ed40f13ed7e3b94e6128
jpena91@gmail.com <jpena91@gmail.com>
2020-09-22 08:19:15 -0400

    add now playing, set params

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [nowPlaying, setNowPlaying] = useState({ name: "none", image: "" });
+  const [loggedIn, setLoggedIn] = useState("");
+  const [params, setParams] = useState();
+
+  const getNowPlaying = () => {
+    spotify.getMyCurrentPlaybackState().then((response) => {
+      if (response) {
+        setNowPlaying({
+          name: response.item.album.name,
+          image: response.image,
+        });
+      } else {
+        setNowPlaying({ name: "Error Getting Information", image: "" });
+      }
+    });
+  };
+
+  useEffect(() => {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    console.log(q);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    if (hashParams.access_token) {
+      setParams(hashParams);
+      spotify.setAccessToken(hashParams.access_token);
+    }
+    return setParams(hashParams);
+  }, [setParams]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      {params ? (
+        ""
+      ) : (
+        <a href="http://localhost:8888">
+          <button>Login With Spotify</button>
         </a>
-      </header>
+      )}
+      <div>Now Playing: {nowPlaying.name}</div>
+      <div>
+        <img src={nowPlaying.image} style={{ width: 100 }} />
+      </div>
+      <button onClick={() => getNowPlaying()}>Check Now Playing</button>
     </div>
   );
 }
